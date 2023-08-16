7b8e1f27d8e370b792f419832b029e054e1d4a95
Senthil Prabu <senthilprabu@heptagon.in>
2023-08-16 14:12:58 +0530

    laravel google chat log

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# hep-laravel-google-chat-log
+<p align="center"><code>&hearts; Made with &lt;love/&gt; And I love &lt;code/&gt;</code></p>
+
+# Laravel/Lumen Google Chat Log
+
+Brings up the option for sending the logs to google chat [GSuite] from [Laravel](https://laravel.com)/[Lumen](https://lumen.laravel.com).
+
+## Installation
+### Composer install
+```shell
+composer require senthilhep/hep-laravel-google-chat-log
+```
+
+Add the following code to the channels array in `config/logging.php` in your laravel/lumen application.
+```
+'google-chat' => [
+    'driver' => 'monolog',
+    'url' => env('LOG_GOOGLE_CHAT_WEBHOOK_URL'),
+    'notify_users' => [
+        'default' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_DEFAULT'),
+        'emergency' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_EMERGENCY'),
+        'alert' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_ALERT'),
+        'critical' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_CRITICAL'),
+        'error' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_ERROR'),
+        'warning' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_WARNING'),
+        'notice' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_NOTICE'),
+        'info' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_INFO'),
+        'debug' => env('LOG_GOOGLE_CHAT_NOTIFY_USER_ID_DEBUG'),
+    ],
+    'level' => 'warning',
+    'handler' => \Enigma\GoogleChatHandler::class,
+],
+```
+
+You can provide the eight logging levels defined in the [RFC 5424 specification](https://tools.ietf.org/html/rfc5424): `emergency`, `alert`, `critical`, `error`, `warning`, `notice`, `info`, and `debug`
+
+<b>Note*:</b> Make sure to set the <b>LOG_GOOGLE_CHAT_WEBHOOK_URL</b> env variable.
+And <b>LOG_GOOGLE_CHAT_NOTIFY_USER_ID</b> is optional.
+Here, you can set multiple google chat webhook url as comma separated value for the <b>LOG_GOOGLE_CHAT_WEBHOOK_URL</b> env variable.
+
+Now, you can notify a specific user with `@mention` in the error log by setting the corresponding USER_ID to the `LOG_GOOGLE_CHAT_NOTIFY_USER_ID_DEFAULT` env variable. User Ids mapped under `LOG_GOOGLE_CHAT_NOTIFY_USER_ID_DEFAULT` will be notified for all log levels.  
+
+For getting the <b>USER_ID</b>, right-click the user-icon of the person whom you want to notify in the Google chat from your browser window and select inspect. Under the `div` element find the attribute data_member_id, then the USER_ID can be found as `data-member-id="user/human/{USER_ID}>"`.
+
+In order to notify all the users like `@all`, Set ```LOG_GOOGLE_CHAT_NOTIFY_USER_ID_DEFAULT=all```. Also, you can set multiple USER_IDs as comma separated value.
+In order to notify different users for different log levels, you can set the corresponding env keys mentioned to configure in the `logging.php` file. 
+
+## License
+
+Copyright © Senthil Prabu
+
+Laravel Google Chat Log is open-sourced software licensed under the [MIT license](LICENSE).
---
+++ src/GoogleChatHandler.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Enigma;
+
+use Exception;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+use Monolog\LogRecord;
+
+class GoogleChatHandler extends AbstractProcessingHandler
+{
+    /**
+     * Writes the record down to the log of the implementing handler.
+     *
+     * @param LogRecord $record
+     *
+     * @throws Exception
+     */
+    protected function write(LogRecord $record): void
+    {
+        $params = $record->toArray();
+        $params['formatted'] = $record->formatted;
+        foreach ($this->getWebhookUrl() as $url) {
+            Log::channel('daily')->error($record->formatted);
+            Http::post($url, $this->getRequestBody($params));
+        }
+    }
+
+    /**
+     * Get the webhook url.
+     *
+     * @return array
+     *
+     * @throws Exception
+     */
+    protected function getWebhookUrl(): array
+    {
+        $url = Config::get('logging.channels.google-chat.url');
+        if (!$url) {
+            throw new Exception('Google chat webhook url is not configured.');
+        }
+
+        if (is_array($url)) {
+            return $url;
+        }
+
+        return array_map(function ($each) {
+            return trim($each);
+        }, explode(',', $url));
+    }
+
+    /**
+     * Get the request body content.
+     *
+     * @param array $recordArr
+     * @return array
+     */
+    protected function getRequestBody(array $recordArr): array
+    {
+        $recordArr['formatted'] = substr($recordArr['formatted'], 34);
+        return [
+            'text' => substr($this->getNotifiableText($recordArr['level'] ?? '') . $recordArr['formatted'], 0, 3800),
+            'cardsV2' => [
+                [
+                    'cardId' => 'info-card-id',
+                    'card' => [
+                        'header' => [
+                            'title' => Config::get('app.name') . ": {$recordArr['level_name']}: {$recordArr['message']}",
+                            'subtitle' => Config::get('app.url'),
+                        ],
+                        'sections' => [
+                            'header' => 'Details',
+                            'collapsible' => true,
+                            'uncollapsibleWidgetsCount' => 1,
+                            'widgets' => [
+                                $this->cardWidget(ucwords(Config::get('app.env') ?? '') . ' [Env]', 'BOOKMARK'),
+                                $this->cardWidget($this->getLevelContent($recordArr), 'TICKET'),
+                                $this->cardWidget($recordArr['datetime'], 'CLOCK'),
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Get the card content.
+     *
+     * @param array $recordArr
+     * @return string
+     */
+    protected function getLevelContent(array $recordArr): string
+    {
+        $color = [
+            Logger::EMERGENCY => '#ff1100',
+            Logger::ALERT => '#ff1100',
+            Logger::CRITICAL => '#ff1100',
+            Logger::ERROR => '#ff1100',
+            Logger::WARNING => '#ffc400',
+            Logger::NOTICE => '#00aeff',
+            Logger::INFO => '#48d62f',
+            Logger::DEBUG => '#000000',
+        ][$recordArr['level']] ?? '#ff1100';
+
+        return "<font color='{$color}'>{$recordArr['level_name']}</font>";
+    }
+
+    /**
+     * Get the text string for notifying the configured user id.
+     *
+     * @param $level
+     * @return string
+     */
+    protected function getNotifiableText($level): string
+    {
+        $levelBasedUserIds = [
+            Logger::EMERGENCY => Config::get('logging.channels.google-chat.notify_users.emergency'),
+            Logger::ALERT => Config::get('logging.channels.google-chat.notify_users.alert'),
+            Logger::CRITICAL => Config::get('logging.channels.google-chat.notify_users.critical'),
+            Logger::ERROR => Config::get('logging.channels.google-chat.notify_users.error'),
+            Logger::WARNING => Config::get('logging.channels.google-chat.notify_users.warning'),
+            Logger::NOTICE => Config::get('logging.channels.google-chat.notify_users.notice'),
+            Logger::INFO => Config::get('logging.channels.google-chat.notify_users.info'),
+            Logger::DEBUG => Config::get('logging.channels.google-chat.notify_users.debug'),
+        ][$level] ?? '';
+
+        $levelBasedUserIds = trim($levelBasedUserIds);
+        if (($userIds = Config::get('logging.channels.google-chat.notify_users.default')) && $levelBasedUserIds) {
+            $levelBasedUserIds = ",$levelBasedUserIds";
+        }
+
+        return $this->constructNotifiableText(trim($userIds) . $levelBasedUserIds);
+    }
+
+    /**
+     * Get the notifiable text for the given userIds String.
+     *
+     * @param $userIds
+     * @return string
+     */
+    protected function constructNotifiableText($userIds): string
+    {
+        if (!$userIds) {
+            return '';
+        }
+
+        $allUsers = '';
+        $otherIds = implode(array_map(function ($userId) use (&$allUsers) {
+            if (strtolower($userId) === 'all') {
+                $allUsers = '<users/all> ';
+                return '';
+            }
+
+            return "<users/$userId> ";
+        }, array_unique(
+                explode(',', $userIds))
+        ));
+
+        return $allUsers . $otherIds;
+    }
+
+    /**
+     * Card widget content.
+     *
+     * @return array[]
+     */
+    public function cardWidget(string $text, string $icon): array
+    {
+        return [
+            'decoratedText' => [
+                'startIcon' => [
+                    'knownIcon' => $icon,
+                ],
+                'text' => $text,
+            ],
+        ];
+    }
+}
